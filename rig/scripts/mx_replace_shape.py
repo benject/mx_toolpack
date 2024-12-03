b2ae9a75b0b44b4fd4fef83382b930a2fffeb382
bing <bing.chen@lilith.com>
2024-12-03 18:24:27 +0800

    fix replace shape

--- rig/scripts/mx_replace_shape.py
+++ rig/scripts/mx_replace_shape.py
@@ -13,5 +13,5 @@ def replace_shape():
         new_shape = cmds.listRelatives(temp_curve)[0]
         old_shape = cmds.listRelatives( curve_xform )[0]
         cmds.delete(old_shape)
-        cmds.parent(new_shape, curve_xform, s=True, r=True)
-        
+        cmds.parent(new_shape, curve_xform, s=True, r=True)    
+        cmds.delete(temp_curve)
